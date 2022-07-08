ebb873321686af718a0add6e4b80274109a40435
Gavin Sawyer <gavin@sawyerfamily.org>
2022-07-08 04:18:56 -0400

    Added Angular Material, Flex Layout. Default style changes and starting to build interface.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +12,10 @@ import { environment } from '../environments/environment';
 import { initializeApp, provideFirebaseApp, FirebaseApp } from '@angular/fire/app';
 import { provideAnalytics, Analytics, getAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 import { provideAuth, Auth, getAuth } from '@angular/fire/auth';
+
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
 	declarations: [
@@ -22,6 +29,13 @@ import { provideAuth, Auth, getAuth } from '@angular/fire/auth';
 		provideFirebaseApp((): FirebaseApp => initializeApp(environment.firebase)),
 		provideAnalytics((): Analytics => getAnalytics()),
 		provideAuth((): Auth => getAuth()),
+		BrowserAnimationsModule,
+
+		FlexLayoutModule,
+
+		MatSidenavModule,
+		MatToolbarModule,
+		MatButtonModule,
 	],
 	providers: [
 		ScreenTrackingService,
